1b8d3b474884b89359518af4d836efb00c7db4e4
Gabriele <dbgabri@gmail.com>
2013-03-19 21:57:54 +0100

    added render.. methos....

--- Randerer.h
+++ Randerer.h
@@ -8,25 +8,33 @@
 
 namespace TinyRasterization {
 
-	struct Vertex3D{
-		
-		Vec3 point;
+	struct Vertex3D{		
+		Vec3  space;
 		Color color;
-		Vec2 uv;
+		struct{ 
+			float u,v;
+		}uv;
+	};
 
+	struct Gradients{
+		Gradients(const Vertex3D* pVertices); //pass 3 Vertex3D points
+		float oneOverZ[3];				//1/z for each vertex
+		float uOverZ[3];				//u/z for each vertex
+		float vOverZ[3];				//v/z for each vertex
+		float dOneOverZdX,dOneOverZdY;	//d(1/z)/dX, d(1/z)/dY
+		float dUOverZdX,dUOverZdY;		//d(u/z)/dX, d(u/z)/dY
+		float dVOverZdX,dVOverZdY;		//d(v/z)/dX, d(v/z)/dY
 	};
 	struct Edge{
 	
-		Vec3 p1,p2;
-		Edge(const Vec3 & p1,const Vec3 & p2){
-			if(p1.y<p2.y){
-				this->p1=p1;
-				this->p2=p2;
-			}
-			else{
-				
-			}
-		}
+		float x,xStep;
+		float y,height;
+		float oneOverZ,oneOverZStep;
+		float uOverZ,uOverZStep;
+		float vOverZ,vOverZStep;
+
+		Edge(const Gradients& gradients,const Vertex3D* pVertices,int top,int bottom );
+		int step();
 	
 	};
 
@@ -53,8 +61,9 @@ namespace TinyRasterization {
 
 	public:
 		
-
+		
 		Randerer(BuffersContext *ctx=NULL):ctx(ctx){}
+		void setBuffersContext(BuffersContext *_ctx=NULL){ ctx=_ctx; }
 		void setViewPort(int x,int y, int width, int hight){
 			viewPort.x=x;
 			viewPort.y=y;
@@ -63,6 +72,17 @@ namespace TinyRasterization {
 			window.x= (viewPort.x + 1) * viewPort.width*0.5;
 			window.y= (viewPort.y + 1) * viewPort.hight*0.5;
 		}
+		//set model matrix
+		void setModelView(const Matrix4x4& mv){
+			modelView=mv;
+			modelViewProjection=projection.mul(modelView);
+		}		
+		//set project matrix
+		void setProjection(const Matrix4x4& pj){
+			projection=pj;
+			modelViewProjection=projection.mul(modelView);
+		}
+		//
 		void drawTriangleWireFrame(const std::vector<Vec3>& tris,
 								   const std::vector<uint>& index,
 								   const Color& color){
@@ -106,7 +126,38 @@ namespace TinyRasterization {
 			}
 
 		}
-
+		//functions
+		void drawTriangleEasy(const std::vector<Vec3>& tris,
+							  const std::vector<uint>& index,
+							  const Color& color){
+			Vec4 clip1,clip2,clip3;
+			Vec3 div1,div2,div3;
+			Vec2 view1,view2,view3;
+			Vertex3D p[3];
+			for(int i=0;i<index.size();i+=3){
+				//clip space
+				clip1=modelViewProjection.mul(Vec4(tris[index[i]],1));
+				clip2=modelViewProjection.mul(Vec4(tris[index[i+1]],1));
+				clip3=modelViewProjection.mul(Vec4(tris[index[i+2]],1));
+				//div space
+				div1 = clip1.xyz()/clip1.w;
+				div2 = clip2.xyz()/clip2.w;
+				div3 = clip3.xyz()/clip3.w;
+				//window space
+				view1=div1.xy()*window;
+				view2=div2.xy()*window;
+				view3=div3.xy()*window;
+				//draw
+				p[1].space=div1;
+				p[2].space=div1;
+				p[3].space=div1;
+				drawTriangle(p);
+			}
+		}
+		void drawTriangle(const Vertex3D *pV /* , Texture& image */);
+		void drawSpanLine(const Gradients&  Gradients,
+						  Edge *pLeft,
+						  Edge *pRight/* , Texture& image */ );
 	};
 
 };
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <VectorialMath.h>
 #include <ApplicationIstance.h>
 #include <Application.h>
+#include <Randerer.h>
 #include <iostream>
 using namespace TinyRasterization;
 
@@ -13,6 +14,7 @@ public:
 								 480, //hight
 								 60   //fps
 								 ){}
+	/*
 	struct Vertex{
 		Vec3 xyz;
 		Vec2 uv;
@@ -22,6 +24,8 @@ public:
 			   
 	};
 	std::vector<Vertex> points;
+	*/
+	std::vector<Vec3> points;
 	std::vector<uint> lineIndex;
 	std::vector<uint> triIndex;
 
@@ -35,13 +39,14 @@ public:
 	//Matrix4x4 model;
 	//Matrix4x4 view;
 	Matrix4x4 proj;
+	Randerer render;
 
 	void start(){
 		
-		points.push_back(Vertex(Vec3(-50 ,0   ,0 ) ,Vec2(0,0)));
-		points.push_back(Vertex(Vec3(50  ,0   ,0 ) ,Vec2(1,0)));
-		points.push_back(Vertex(Vec3(0   ,50  ,0 ) ,Vec2(0.5,1)));
-		points.push_back(Vertex(Vec3(0   ,0   ,50) ,Vec2(0.5,1)));
+		points.push_back(Vec3(-50 ,0   ,0 ));
+		points.push_back(Vec3(50  ,0   ,0 ));
+		points.push_back(Vec3(0   ,50  ,0 ));
+		points.push_back(Vec3(0   ,0   ,50));
 
 		//tri-line
 		lineIndex.push_back(0); lineIndex.push_back(1); 
@@ -66,7 +71,6 @@ public:
 		Vec2 a(1,2);
 		Vec2 b(3,4);
 		Math::swap(a,b);
-		//triangles
 		/*
 		tri.v1.xyz=Vec3(-50 ,50  ,0);
 		tri.v2.xyz=Vec3(50  ,50  ,0);
@@ -80,6 +84,7 @@ public:
 
 		//set matrix
 		proj.setPerspective(45.0f, 840.0f/480.0f ,0.1f, 10.0f);
+		render.setProjection(proj);
 		mov=Vec3(0,0,200);
 		mov2=Vec3(0,0,300);
 
@@ -87,7 +92,7 @@ public:
 
 	void update(BuffersContext *ctx,float dt){
 
-
+		render.setBuffersContext(ctx);
 		
 		if (GetAsyncKeyState('W') & 0x8000) mov.y-=1;
 		if (GetAsyncKeyState('S') & 0x8000) mov.y+=1;
@@ -131,58 +136,16 @@ public:
 		ctx->clear();
 		//offset triangle
 		mT.setTranslation(mov2);
-		drawModelVireWare(ctx,proj.mul(mT.mul(quad.getMatrix())),Color(255,0,0,255));
+		render.setModelView(mT.mul(quad.getMatrix()));
+		render.drawTriangleWireFrame(points,triIndex,Color(255,0,0,255));
 		//draw
 		mT.setTranslation(mov);
-		drawModelVireWare(ctx,proj.mul(mT.mul(quad.getMatrix())),Color(255,0,0,255));
+		render.setModelView(mT.mul(quad.getMatrix()));
+		render.drawTriangleWireFrame(points,triIndex,Color(255,0,0,255));
 		//
 
 	}
 	void end(){
-
-	}
-	void drawModelVireWare(BuffersContext *ctx,const Matrix4x4& projectViewModel,const Color& color){
-			Vec4 vClip1,vClip2,vClip3;	
-			//for each vertex		
-			for(int i=0;i<triIndex.size();i+=3){
-				//clip space
-				vClip1=projectViewModel.mul(Vec4(points[triIndex[i]].xyz,1));
-				vClip2=projectViewModel.mul(Vec4(points[triIndex[i+1]].xyz,1));
-				vClip3=projectViewModel.mul(Vec4(points[triIndex[i+2]].xyz,1));
-				//div space
-				Vec3 vDiv1 = vClip1.xyz()/vClip1.w;
-				Vec3 vDiv2 = vClip2.xyz()/vClip2.w;
-				Vec3 vDiv3 = vClip3.xyz()/vClip3.w;
-				//window space
-				Vec2 vView1=vDiv1.xy()*Vec2(getWidth(),getHight())+Vec2(getWidth()/2.0,getHight()/2.0);
-				Vec2 vView2=vDiv2.xy()*Vec2(getWidth(),getHight())+Vec2(getWidth()/2.0,getHight()/2.0);
-				Vec2 vView3=vDiv3.xy()*Vec2(getWidth(),getHight())+Vec2(getWidth()/2.0,getHight()/2.0);
-				//draw
-				drawLine(ctx,Color(0,0,255,255),vView1,vView2);
-				drawLine(ctx,Color(0,0,255,255),vView2,vView3);
-				drawLine(ctx,Color(0,0,255,255),vView3,vView1);
-			}
-	}
-	
-
-	//function
-	void drawLine(BuffersContext *ctx,
-				  const Color& color,
-				  Vec2 v1,Vec2 v2){
-	 
-	  int x0=v1.x, y0=v1.y, 
-		  x1=v2.x, y1=v2.y;
-	  int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
-	  int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
-	  int err = (dx>dy ? dx : -dy)/2, e2=0;
- 
-	  for(;;){
-		ctx->setPixel(color,x0,y0);
-		if (x0==x1 && y0==y1) break;
-		e2 = err;
-		if (e2 >-dx) { err -= dy; x0 += sx; }
-		if (e2 < dy) { err += dx; y0 += sy; }
-	  }
 
 	}
 
